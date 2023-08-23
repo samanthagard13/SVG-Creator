d4fe388f7af6de2744999039676aa338ac6ef37f
samantha gard <samantha.gard13@gmail.com>
2023-08-23 11:40:31 -0400

    changes to test file for functionality and additional tests for shape changes

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const generateSVG = ({ characters, textColor, shape, shapeColor }) => {
     width="300" height="200"
     xmlns="http://www.w3.org/2000/svg">
 
-<rect width="100%" height="100%" fill="white" />
+    <rect width="100%" height="100%" fill="white" />
     
     ${shapes.createShape(shape, shapeColor)}
 
